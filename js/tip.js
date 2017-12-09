b290f3c7d607cd344cf6d3eba2048f5076f3ca1b
Eugene Letunov <defrazo@yandex.ru>
2017-12-10 02:36:36 +0500

    End of Refactoring

--- js/tip.js
+++ js/tip.js
@@ -1,12 +1,16 @@
+$(function () { //Инициализирует подсказку
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
 $(document).ready(function() { //Отображает подсказку
-  $(function() {
-    $("[data-tooltip]").mousemove(function(eventObject) {
-      $data_tooltip = $(this).attr("data-tooltip");
-      $("#replaceble_block").hide();
-      $("#tooltip").text($data_tooltip).show();
-    }).mouseout(function() {
-      $("#replaceble_block").show();
-      $("#tooltip").hide().text("")
+    $(function() {
+        $("[data-tooltip]").mousemove(function(eventObject) {
+            $data_tooltip = $(this).attr("data-tooltip");
+            $("#replaceble_block").hide();
+            $("#tooltip").text($data_tooltip).show();
+        }).mouseout(function() {
+            $("#replaceble_block").show();
+            $("#tooltip").hide().text("")
+        });
     });
-  });
 });
