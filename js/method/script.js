90f723b2afdfe494c21a55173afcb6056cc0736e
Eugene Letunov <defrazo@yandex.ru>
2018-01-21 02:17:23 +0500

    Added options

--- js/method/script.js
+++ js/method/script.js
@@ -6,4 +6,25 @@ $(document).ready(function() {
 			$('.stopwatch_container').css("display", "none");
 		}
 	});
+
+	$('#options_button').on('change', function() { 
+		if ($("#options_button").prop("checked")) {  
+			$('.options_container').css("display", "block");
+		} else {
+			$('.options_container').css("display", "none");
+		}
+	});
+
+	$('#fs14px').on('click', function(){
+		$('body').addClass('.fs14px');
+	})
+
+	var newValue;
+	$('#styleSwitch').change(function () {
+	if (newValue)
+		newValue.css('color', 'black');
+		
+	newValue = $(this).find(':selected');
+	newValue.css('color', 'red');
+	});
 });
