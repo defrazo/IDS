26d098b3d6d170c21fc21dedf63fe16f3c6495e7
Eugene Letunov <defrazo@inbox.ru>
2018-05-26 18:36:49 +0500

    added

---
+++ applications/section4/selfrate/js/method/writeOn.js
@@ -0,0 +1,146 @@
+function get_data() { //Вносит заполненные данные из полей формы в localStorage
+	
+	var date = $('#date').val();
+	if (date != '') localStorage.setItem('dateinp', date);
+
+	var gender = $('input[name="gender"]:checked').val();
+	if (gender != '') localStorage.setItem('genderinp', gender);
+
+	var sname = $('#sname').val();
+	if (sname != '') localStorage.setItem('snameinp', sname);
+	
+	var fname = $('#fname').val();
+	if (fname != '') localStorage.setItem('fnameinp', fname);
+
+	var mname = $('#mname').val();
+	if (mname != '') localStorage.setItem('mnameinp', mname);
+
+	var fac = $('#fac').val();
+	if (fac != '') localStorage.setItem('facinp', fac);
+
+	var p11 = $('#p11').val();
+	if (p11 != '') localStorage.setItem('p11inp', p11);
+
+	var p12 = $('#p12').val();
+	if (p12 != '') localStorage.setItem('p12inp', p12);
+
+	var p21 = $('#p21').val();
+	if (p21 != '') localStorage.setItem('p21inp', p21);
+
+	var p22 = $('#p22').val();
+	if (p22 != '') localStorage.setItem('p22inp', p22);
+
+	var p31 = $('#p31').val();
+	if (p31 != '') localStorage.setItem('p31inp', p31);
+
+	var p32 = $('#p32').val();
+	if (p32 != '') localStorage.setItem('p32inp', p32);
+
+	var p41 = $('#p41').val();
+	if (p41 != '') localStorage.setItem('p41inp', p41);
+
+	var p42 = $('#p42').val();
+	if (p42 != '') localStorage.setItem('p42inp', p42);
+
+	var p51 = $('#p51').val();
+	if (p51 != '') localStorage.setItem('p51inp', p51);
+
+	var p52 = $('#p52').val();
+	if (p52 != '') localStorage.setItem('p52inp', p52);
+
+	var p61 = $('#p61').val();
+	if (p61 != '') localStorage.setItem('p61inp', p61);
+
+	var p62 = $('#p62').val();
+	if (p62 != '') localStorage.setItem('p62inp', p62);
+
+	var p71 = $('#p71').val();
+	if (p71 != '') localStorage.setItem('p71inp', p71);
+
+	var p72 = $('#p72').val();
+	if (p72 != '') localStorage.setItem('p72inp', p72);
+
+	var p81 = $('#p81').val();
+	if (p81 != '') localStorage.setItem('p81inp', p81);
+
+	var p82 = $('#p82').val();
+	if (p82 != '') localStorage.setItem('p82inp', p82);
+
+	var p91 = $('#p91').val();
+	if (p91 != '') localStorage.setItem('p91inp', p91);
+
+	var p92 = $('#p92').val();
+	if (p92 != '') localStorage.setItem('p92inp', p92);
+
+	var p101 = $('#p101').val();
+	if (p101 != '') localStorage.setItem('p101inp', p101);
+
+	var p102 = $('#p102').val();
+	if (p102 != '') localStorage.setItem('p102inp', p102);
+
+	var p111 = $('#p111').val();
+	if (p111 != '') localStorage.setItem('p111inp', p111);
+
+	var p112 = $('#p112').val();
+	if (p112 != '') localStorage.setItem('p112inp', p112);
+
+	var p121 = $('#p121').val();
+	if (p121 != '') localStorage.setItem('p121inp', p121);
+
+	var p122 = $('#p122').val();
+	if (p122 != '') localStorage.setItem('p122inp', p122);
+
+	var p131 = $('#p131').val();
+	if (p131 != '') localStorage.setItem('p131inp', p131);
+
+	var p132 = $('#p132').val();
+	if (p132 != '') localStorage.setItem('p132inp', p132);
+
+	var p141 = $('#p141').val();
+	if (p141 != '') localStorage.setItem('p141inp', p141);
+
+	var p142 = $('#p142').val();
+	if (p142 != '') localStorage.setItem('p142inp', p142);
+
+	var p151 = $('#p151').val();
+	if (p151 != '') localStorage.setItem('p151inp', p151);
+
+	var p152 = $('#p152').val();
+	if (p152 != '') localStorage.setItem('p152inp', p152);
+
+	var p151 = $('#p151').val();
+	if (p151 != '') localStorage.setItem('p151inp', p151);
+
+	var p152 = $('#p152').val();
+	if (p152 != '') localStorage.setItem('p152inp', p152);
+
+	var p161 = $('#p161').val();
+	if (p161 != '') localStorage.setItem('p161inp', p161);
+
+	var p162 = $('#p162').val();
+	if (p162 != '') localStorage.setItem('p162inp', p162);
+
+	var p171 = $('#p171').val();
+	if (p171 != '') localStorage.setItem('p171inp', p171);
+
+	var p172 = $('#p172').val();
+	if (p172 != '') localStorage.setItem('p172inp', p172);
+
+	var p181 = $('#p181').val();
+	if (p181 != '') localStorage.setItem('p181inp', p181);
+
+	var p182 = $('#p182').val();
+	if (p182 != '') localStorage.setItem('p182inp', p182);
+
+	var p191 = $('#p191').val();
+	if (p191 != '') localStorage.setItem('p191inp', p191);
+
+	var p192 = $('#p192').val();
+	if (p192 != '') localStorage.setItem('p192inp', p192);
+
+	var p201 = $('#p201').val();
+	if (p201 != '') localStorage.setItem('p201inp', p201);
+
+	var p202 = $('#p202').val();
+	if (p202 != '') localStorage.setItem('p202inp', p202);
+};
