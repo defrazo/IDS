a0fefab103c1764bf01a3f657ce5ee2db01ce417
Eugene Letunov <defrazo@inbox.ru>
2018-06-04 16:13:16 +0500

    Ended shultes

---
+++ applications/section4/shulte2/js/method/writeOn.js
@@ -0,0 +1,20 @@
+function get_data() { //Вносит заполненные данные из полей формы в localStorage
+	
+	var date = $('#date').val();
+	if (date != '') localStorage.setItem('dateinp', date);
+
+	var gender = $('input[name="gender"]:checked').val();
+	if (gender != '') localStorage.setItem('genderinp', gender);
+
+	var sname = $('#sname').val();
+	if (sname != '') localStorage.setItem('snameinp', sname);
+	
+	var fname = $('#fname').val();
+	if (fname != '') localStorage.setItem('fnameinp', fname);
+
+	var mname = $('#mname').val();
+	if (mname != '') localStorage.setItem('mnameinp', mname);
+
+	var fac = $('#fac').val();
+	if (fac != '') localStorage.setItem('facinp', fac);
+};
